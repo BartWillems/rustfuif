7749076cc8c489a1648ed43059cae5ff645cfc44
Bart Willems <bwillems@protonmail.com>
2020-04-23 11:57:18 +0200

    return the user object as json when registering
    Signed-off-by: Bart Willems <bwillems@protonmail.com>

--- src/auth/routes.rs
+++ src/auth/routes.rs
@@ -5,20 +5,19 @@ use crate::users::{User, UserMessage};
 use crate::validator::Validator;
 
 use actix_identity::Identity;
-use actix_web::http::StatusCode;
 use actix_web::web::{Data, Json};
 use actix_web::{post, web, HttpResponse};
 use serde_json::json;
 
 #[post("/register")]
 async fn create_account(user: Json<Validator<UserMessage>>, pool: Data<db::Pool>) -> Response {
-    web::block(move || {
+    let user: User = web::block(move || {
         let conn = pool.get()?;
         User::create(&mut user.into_inner().validate()?, &conn)
     })
     .await?;
 
-    Ok(HttpResponse::new(StatusCode::OK))
+    http_created_json!(user);
 }
 
 #[post("/login")]
